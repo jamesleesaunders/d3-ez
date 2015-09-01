d1e67d3c90702951632f038362a2d93350f9988c
James Saunders <james@saunders-family.net>
2015-09-01 20:45:38 +0100

    Test File Loader

--- js/d3.ez.js
+++ js/d3.ez.js
@@ -20,7 +20,7 @@ function include(file) {
     document.write('<' + 'script src="' + src + '"' + ' type="' + type + '"><' + '/script>');
 }
 
-function includeV2(file) {
+function include2(file) {
 	// Method 2 - Using document.createElement
     var head = document.getElementsByTagName('head')[0];
     var script = document.createElement('script');
@@ -30,10 +30,12 @@ function includeV2(file) {
 
 	script.src = path + file;
     script.type = 'text/javascript';
+    script.async = false;
+    script.onload = function() { console.log(file); };
     head.appendChild(script);
 }
 
-function includeV3(file) {
+function include3(file) {
 	// Method 3 - Using D3
 	var loc = document.currentScript.src
 	var path = loc.substring(0, loc.lastIndexOf("/") + 1);
@@ -41,7 +43,9 @@ function includeV3(file) {
 	var type = 'text/javascript';
 	
 	d3.select('head')
-		.append("script")
+		.append('script')
 		.attr('src', src)
-		.attr('type', type);
+		.attr('type', type)
+		.attr('async', false)
+		.attr('onload', function() { console.log(file); });
 }
