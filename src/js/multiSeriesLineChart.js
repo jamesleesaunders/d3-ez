0e86f3a4400dece969f93d597b4454b6a4c8459f
James Saunders <james@saunders-family.net>
2017-09-10 21:23:47 +0100

    Multi Series Line Chart.

--- src/js/multiSeriesLineChart.js
+++ src/js/multiSeriesLineChart.js
@@ -4,9 +4,7 @@
  * @example
  * var myChart = d3.ez.multiSeriesLineChart()
  *     .width(400)
- *     .height(300)
- *     .transition({ease: "bounce", duration: 1500})
- *     .groupType("stacked");
+ *     .height(300);
  * d3.select("#chartholder")
  *     .datum(data)
  *     .call(myChart);
@@ -20,20 +18,14 @@ d3.ez.multiSeriesLineChart = function module() {
   var width = 400;
   var height = 300;
   var margin = { top: 20, right: 20, bottom: 20, left: 40 };
-  var transition = { ease: "bounce", duration: 500 };
   var classed = "multiSeriesLineChart";
   var colors = d3.ez.colors.categorical(4);
-  var gap = 0;
   var yAxisLabel = null;
   var groupType = "clustered";
 
   // Data Options (Populated by 'init' function)
   var chartW = 0;
   var chartH = 0;
-  var groupNames = undefined;
-  var categoryNames = [];
-  var categoryTotals = [];
-  var groupTotals = [];
   var maxValue = 0;
   var maxGroupTotal = undefined;
   var xScale = undefined;
@@ -45,42 +37,90 @@ d3.ez.multiSeriesLineChart = function module() {
   // Dispatch (Custom events)
   var dispatch = d3.dispatch("customMouseOver", "customMouseOut", "customClick");
 
+  // Other functions
+  var parseDate = d3.time.format("%Y%m%d").parse;
+
+  var line = undefined;
+
   function init(data) {
+    chartW = width - margin.left - margin.right;
+    chartH = height - margin.top - margin.bottom;
 
 
+    // Colour Scale
+    colorScale = d3.scale.category10()
+      .domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
+
+    // X & Y Scales
+    xScale = d3.time.scale()
+      .range([0, chartW]);
+    yScale = d3.scale.linear()
+      .range([chartH, 0]);
+
+    // X & Y Axis
+    xAxis = d3.svg.axis()
+      .scale(xScale)
+      .orient("bottom");
+    yAxis = d3.svg.axis()
+      .scale(yScale)
+      .orient("left");
+
+    line = d3.svg.line()
+      .x(function(d) { return xScale(d.date); })
+      .y(function(d) { return yScale(d.temperature); });
   }
 
   function my(selection) {
     selection.each(function(data) {
-      var margin = { top: 20, right: 80, bottom: 30, left: 50 },
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
-      var parseDate = d3.time.format("%Y%m%d").parse;
-      var x = d3.time.scale()
-        .range([0, width]);
-      var y = d3.scale.linear()
-        .range([height, 0]);
-      var color = d3.scale.category10();
-      var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
-      var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
-      var line = d3.svg.line()
-        .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.temperature); });
-      var svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      // Initialise Data
+      init(data);
 
-      color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
+
+
+
+
+
+      // Create SVG and Chart containers (if they do not already exist)
+      if (!svg) {
+        svg = (function(selection) {
+          var el = selection[0][0];
+          if (!!el.ownerSVGElement || el.tagName === "svg") {
+            return selection;
+          } else {
+            return selection.append("svg");
+          }
+        })(d3.select(this));
+
+        svg.classed("d3ez", true)
+          .attr({ width: width, height: height });
+
+        chart = svg.append("g").classed("chart", true);
+        chart.append("g").classed("x-axis axis", true);
+        chart.append("g").classed("y-axis axis", true)
+          .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", -35)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text(yAxisLabel);
+      } else {
+        chart = selection.select(".chart");
+      }
+
+
+      // Update the chart dimensions
+      chart.classed(classed, true)
+        .attr({ width: width, height: height })
+        .attr({ transform: "translate(" + margin.left + "," + margin.top + ")" });
+
+
+
+
+
       data.forEach(function(d) {
         d.date = parseDate(d.date);
       });
-      var cities = color.domain().map(function(name) {
+      var cities = colorScale.domain().map(function(name) {
         return {
           name: name,
           values: data.map(function(d) {
@@ -88,17 +128,18 @@ d3.ez.multiSeriesLineChart = function module() {
           })
         };
       });
-      x.domain(d3.extent(data, function(d) { return d.date; }));
-      y.domain([
+      xScale.domain(d3.extent(data, function(d) { return d.date; }));
+      yScale.domain([
         d3.min(cities, function(c) { return d3.min(c.values, function(v) { return v.temperature; }); }),
         d3.max(cities, function(c) { return d3.max(c.values, function(v) { return v.temperature; }); })
       ]);
 
-      svg.append("g")
+      chart.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
+        .attr("transform", "translate(0," + chartH + ")")
         .call(xAxis);
-      svg.append("g")
+
+      chart.append("g")
         .attr("class", "y axis")
         .call(yAxis)
         .append("text")
@@ -107,28 +148,32 @@ d3.ez.multiSeriesLineChart = function module() {
         .attr("dy", ".71em")
         .style("text-anchor", "end")
         .text("Temperature (ºF)");
-      var city = svg.selectAll(".city")
+
+      var city = chart.selectAll(".city")
         .data(cities)
         .enter().append("g")
         .attr("class", "city");
+
       city.append("path")
         .attr("class", "line")
         .attr("d", function(d) { return line(d.values); })
-        .style("stroke", function(d) { return color(d.name); });
+        .style("stroke", function(d) { return colorScale(d.name); });
+
       city.append("text")
         .datum(function(d) { return { name: d.name, value: d.values[d.values.length - 1] }; })
-        .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
+        .attr("transform", function(d) { return "translate(" + xScale(d.value.date) + "," + yScale(d.value.temperature) + ")"; })
         .attr("x", 3)
         .attr("dy", ".35em")
         .text(function(d) { return d.name; });
+
       city.selectAll("circle")
         .data(function(d) { return d.values })
         .enter()
         .append("circle")
         .attr("r", 3)
-        .attr("cx", function(d) { return x(d.date); })
-        .attr("cy", function(d) { return y(d.temperature); })
-        .style("fill", function(d, i, j) { return color(cities[j].name); });
+        .attr("cx", function(d) { return xScale(d.date); })
+        .attr("cy", function(d) { return yScale(d.temperature); })
+        .style("fill", function(d, i, j) { return colorScale(cities[j].name); });
 
     });
   }
