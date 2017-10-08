567a6db345d5a765143fae915649c54a26cac626
James Saunders <james@saunders-family.net>
2017-10-08 22:13:54 +0100

    Experimenting with breaking into more components. Barchart.

--- src/component.js
+++ src/component.js
@@ -422,3 +422,99 @@ d3.ez.component.legend = function module() {
 
   return my;
 };
+
+/**
+ * Title
+ *
+ * @example
+ * var myBars = d3.ez.barChart()
+ *     .colorScale(**D3 Scale Object**);
+ * d3.select("svg").call(myBars);
+ */
+d3.ez.component.barChart = function module() {
+  // Default Options (Configurable via setters)
+  var height = 100;
+  var width = 300;
+  var colorScale = undefined;
+  var xScale = undefined;
+  var yScale = undefined;
+  var gap = 0;
+  var transition = { ease: d3.easeBounce, duration: 500 };
+  var dispatch = d3.dispatch("customMouseOver", "customMouseOut", "customClick");
+
+  function my(selection) {
+
+    // Add bars to the chart
+    var gapSize = xScale.bandwidth() / 100 * gap;
+    var barW = xScale.bandwidth() - gapSize;
+
+    barGroup = selection.append("g").classed('barGroup', true);
+    var bars = barGroup.selectAll(".bar")
+      .data(data.values);
+
+    bars.enter().append("rect")
+      .attr("class", function(d) { return d.key + " bar"; })
+      .attr("fill", function(d) { return colorScale(d.key); })
+      .attr("width", barW)
+      .attr("x", function(d, i) { return xScale(d.key) + gapSize / 2; })
+      .attr("y", height)
+      .attr("height", 0)
+      .on("mouseover", function(d) { dispatch.call("customMouseOver", this, d); })
+      .merge(bars)
+      .transition()
+      .ease(transition.ease)
+      .duration(transition.duration)
+      .attr("x", function(d, i) { return xScale(d.key) + gapSize / 2; })
+      .attr("y", function(d, i) { return yScale(d.value); })
+      .attr("height", function(d, i) { return height - yScale(d.value); });
+
+    bars.exit()
+      .transition()
+      .style("opacity", 0)
+      .remove();
+  }
+
+  // Configuration Getters & Setters
+  my.height = function(_) {
+    if (!arguments.length) return height;
+    height = _;
+    return this;
+  };
+
+  my.width = function(_) {
+    if (!arguments.length) return width;
+    width = _;
+    return this;
+  };
+
+  my.colorScale = function(_) {
+    if (!arguments.length) return colorScale;
+    colorScale = _;
+    return my;
+  };
+
+  my.xScale = function(_) {
+    if (!arguments.length) return xScale;
+    xScale = _;
+    return my;
+  };
+
+  my.yScale = function(_) {
+    if (!arguments.length) return yScale;
+    yScale = _;
+    return my;
+  };
+
+  my.dispatch = function(_) {
+    if (!arguments.length) return dispatch();
+    dispatch = _;
+    return this;
+  };
+
+  my.on = function() {
+    var value = dispatch.on.apply(dispatch, arguments);
+    return value === dispatch ? my : value;
+  };
+
+  return my;
+};
