9971e2201e5dc2e4d27538a1ed5797c385e16e01
James Saunders <james.saunders@virginmedia.co.uk>
2018-01-18 10:21:13 +0000

    Bump to v2.2.3

--- build/d3-ez.js
+++ build/d3-ez.js
@@ -2,13 +2,13 @@
  * d3-ez
  *
  * @author James Saunders [james@saunders-family.net]
- * @copyright Copyright (C) 2017 James Saunders
+ * @copyright Copyright (C) 2018 James Saunders
  * @license GPLv3
  */
 d3.ez = {
-    version: "2.2.2",
+    version: "2.2.3",
     author: "James Saunders",
-    copyright: "Copyright (C) 2017 James Saunders",
+    copyright: "Copyright (C) 2018 James Saunders",
     license: "GPL-3.0"
 };
 
