7cf69f7ee80da892d14401bae2f87f9dcbf732e6
James Saunders <james@saunders-family.net>
2018-02-20 20:26:18 +0000

    Bump to v3.0.0

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ import { default as chartPolarAreaChart } from "./src/chart/polarAreaChart";
 import { default as chartPunchCard } from "./src/chart/punchCard";
 
 var my = {
-  version: "2.2.6",
+  version: "3.0.0",
   author: "James Saunders",
   copyright: "Copyright (C) 2018 James Saunders",
   license: "GPL-3.0",
