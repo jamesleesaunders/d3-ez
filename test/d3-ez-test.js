b991fc00c8d1bf6a0b47ca031c26517b77ec4b00
James Saunders <james@saunders-family.net>
2018-02-22 22:47:26 +0000

    Added unit test.

--- test/d3-ez-test.js
+++ test/d3-ez-test.js
@@ -1,38 +1,43 @@
+var me = require("../");
 var tape = require("tape");
-var d3 = require("../");
-global.d3 = require("d3");
+var jsdom = require("jsdom");
+var d3 = require("d3");
+global.d3 = d3;
 
 tape("Return d3-ez author and license", function(test) {
-  test.equal(d3.ez.author, 'James Saunders');
-  test.equal(d3.ez.license, 'GPL-3.0');
+  test.equal(me.ez.author, "James Saunders", "Returns author");
+  test.equal(me.ez.license, "GPL-3.0", "Returns license");
 
   test.end();
 });
 
 tape("Test title component", function(test) {
-  var title = d3.ez.component.title().mainText("Foo").subText("Bar");
-  test.equal(title.mainText(), "Foo");
-  test.equal(title.subText(), "Bar");
+  var title = me.ez.component.title().mainText("Foo").subText("Bar");
+  test.equal(title.mainText(), "Foo", "Returns main title");
+  test.equal(title.subText(), "Bar", "Returns sub-title");
 
   test.end();
 });
 
 tape("Test legend component", function(test) {
-  var legend = d3.ez.component.legend().title("Foo Bar");
-  test.equal(legend.title(), "Foo Bar");
+  var legend = me.ez.component.legend().title("Foo Bar");
+  test.equal(legend.title(), "Foo Bar", "Returns legend title");
 
   test.end();
 });
 
 tape("Test colors module", function(test) {
-  var colors1 = d3.ez.colors.categorical(1);
-  test.deepEqual(colors1, ['#5da5da', '#faa43a', '#60bd68', '#f17cb0', '#b2912f', '#b276b2', '#decf3f', '#f15854', '#4d4d4d']);
+  var colors1 = me.ez.colors.categorical(1);
+  var expected1 = ['#5da5da', '#faa43a', '#60bd68', '#f17cb0', '#b2912f', '#b276b2', '#decf3f', '#f15854', '#4d4d4d'];
+  test.deepEqual(colors1, expected1, "Catrgorical colours");
 
-  var colors2 = d3.ez.colors.sequential("#5da5da", 6);
-  test.deepEqual(colors2, ['#417499', '#4c87b3', '#579bcd', '#63afe7', '#6ec3ff', '#79d7ff']);
+  var colors2 = me.ez.colors.sequential("#5da5da", 6);
+  var expected2 = ['#417499', '#4c87b3', '#579bcd', '#63afe7', '#6ec3ff', '#79d7ff'];
+  test.deepEqual(colors2, expected2, "Sequencial colours");
 
-  var colors3 = d3.ez.colors.lumShift(['#5da5da', '#faa43a'], 1);
-  test.deepEqual(colors3, ['#baffff', '#ffff74']);
+  var colors3 = me.ez.colors.lumShift(['#5da5da', '#faa43a'], 1);
+  var expected3 = ['#baffff', '#ffff74'];
+  test.deepEqual(colors3, expected3, "Lumshift colours");
 
   test.end();
 });
@@ -47,7 +52,7 @@ tape("Test dataParse module", function(test) {
       { key: "Bananas", value: 7 }
 		]
   };
-  var result = d3.ez.dataParse(data);
+  var result = me.ez.dataParse(data);
   var expected = {
     levels: 1,
     groupName: 'Fruit',
@@ -63,7 +68,24 @@ tape("Test dataParse module", function(test) {
     maxDecimalPlace: 0,
     thresholds: ['4', '5', '6', '8']
   };
-  test.deepEqual(result, expected);
+  test.deepEqual(result, expected, "Parses data analysis");
+
+  test.end();
+});
+
+tape("Test jsdom", function(test) {
+  var html = "<html><body><div id='chartholder'></div></body></html>";
+  var JSDOM = jsdom.JSDOM;
+  global.document = new JSDOM(html).window.document;
+
+  var chartholder = d3.select("#chartholder")
+    .append("svg");
+  var dot = chartholder.append("circle")
+    .attr("cx", 5)
+    .attr("cy", 10);
+
+  test.equal(+dot.attr("cx"), 5, "Test cx");
+  test.equal(+dot.attr("cy"), 10, "Test cy");
 
   test.end();
 });
