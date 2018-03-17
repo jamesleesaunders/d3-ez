4e23d577ba97d3f8bb45ebbf98c76f2ba7667d71
James Saunders <james@saunders-family.net>
2018-03-17 20:33:41 +0000

    Bump to v3.2.1

--- test/d3-ez-test.js
+++ test/d3-ez-test.js
@@ -1,17 +1,17 @@
-var me = require("../");
+var ez = require("../");
 var tape = require("tape");
 var jsdom = require("jsdom");
 var d3 = require("d3");
 
 tape("Return d3-ez author and license", function(test) {
-  test.equal(me.ez.author, "James Saunders", "Returns author");
-  test.equal(me.ez.license, "GPL-3.0", "Returns license");
+  test.equal(ez.ez.author, "James Saunders", "Returns author");
+  test.equal(ez.ez.license, "GPL-3.0", "Returns license");
 
   test.end();
 });
 
 tape("Test title component", function(test) {
-  var title = me.ez.component.title().mainText("Foo").subText("Bar");
+  var title = ez.ez.component.title().mainText("Foo").subText("Bar");
   test.equal(title.mainText(), "Foo", "Returns main title");
   test.equal(title.subText(), "Bar", "Returns sub-title");
 
@@ -19,22 +19,22 @@ tape("Test title component", function(test) {
 });
 
 tape("Test legend component", function(test) {
-  var legend = me.ez.component.legend().title("Foo Bar");
+  var legend = ez.ez.component.legend().title("Foo Bar");
   test.equal(legend.title(), "Foo Bar", "Returns legend title");
 
   test.end();
 });
 
 tape("Test palette module", function(test) {
-  var palette1 = me.ez.palette.categorical(1);
+  var palette1 = ez.ez.palette.categorical(1);
   var expected1 = ['#5da5da', '#faa43a', '#60bd68', '#f17cb0', '#b2912f', '#b276b2', '#decf3f', '#f15854', '#4d4d4d'];
   test.deepEqual(palette1, expected1, "Catrgorical palette");
 
-  var palette2 = me.ez.palette.sequential("#5da5da", 6);
+  var palette2 = ez.ez.palette.sequential("#5da5da", 6);
   var expected2 = ['#417499', '#4c87b3', '#579bcd', '#63afe7', '#6ec3ff', '#79d7ff'];
   test.deepEqual(palette2, expected2, "Sequencial palette");
 
-  var palette3 = me.ez.palette.lumShift(['#5da5da', '#faa43a'], 1);
+  var palette3 = ez.ez.palette.lumShift(['#5da5da', '#faa43a'], 1);
   var expected3 = ['#baffff', '#ffff74'];
   test.deepEqual(palette3, expected3, "Lumshift palette");
 
@@ -51,7 +51,7 @@ tape("Test dataParse module", function(test) {
       { key: "Bananas", value: 7 }
 		]
   };
-  var result = me.ez.dataParse(data);
+  var result = ez.ez.dataParse(data);
   var expected = {
     levels: 1,
     groupName: 'Fruit',
